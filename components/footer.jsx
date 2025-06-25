1e1835a80cfaa4c41c98c0872e8d6d094fcc80bb
aryamaan-exe <aryamaan.27@gmail.com>
2025-06-25 15:17:23 +0530

    changed footer and color scheme

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,10 +4,16 @@ import { HeartFilledIcon } from "@/components/icons";
 export function Footer() {
     return (
         <>
-            <Divider className="mt-8" />
-            <div className="m-16 flex">
-                Made with <HeartFilledIcon className="p-1" /> by ari. Social links in navbar.
-                Copyright &copy; ari 2025. All rights reserved. Some rights reversed.
+            <div className="bg-slate-900 h-32">
+                <div className="mt-16">
+                    <div className="p-8">
+                        <p className="flex">
+                            Made with <HeartFilledIcon className="p-1" /> by ari. Social links in navbar.
+                            Copyright &copy; ari {new Date().getFullYear()}. All rights reserved. Some rights reversed.
+                        </p>
+                        <p>I hope you vote for my project!</p>
+                    </div>
+                </div>
             </div>
         </>
     );
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -20,33 +20,11 @@ import { ThemeSwitch } from "@/components/theme-switch";
 import {
   GithubIcon,
   DiscordIcon,
-  SearchIcon,
 } from "@/components/icons";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
-    <HeroUINavbar maxWidth="xl" position="sticky">
+    <HeroUINavbar maxWidth="xl" position="sticky" className="bg-slate-900">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
